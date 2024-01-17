1a49f8bc9a2700baba428ef1ca75289fa94a1be2
Robowa <marceli3@outlook.es>
2024-01-17 19:41:32 -0300

    Se agrego herencia.py
    Se agrego el file herencia.py y se hizo una practica de Herencia Jerárquica.

---
+++ herencia.py
@@ -0,0 +1,28 @@
+class Persona:
+    def __init__(self, nombre: str, edad: int, nacionalidad: str):
+        self.nombre = nombre
+        self.edad = edad
+        self.nacionalidad = nacionalidad
+        
+    def hablar(self):
+        print("Hola, estoy hablando un poco")
+        
+class Empleado(Persona):
+    def __init__(self, nombre: str, edad: int, nacionalidad: str, trabajo: str, salario: float):
+        super().__init__(nombre, edad, nacionalidad)
+        self.trabajo = trabajo
+        self.salario = salario
+        
+    #La herencia permite sobreescribir las funciones    
+    def hablar(self):
+        print("Hola, estoy hablando mucho")
+
+class Estudiante(Persona):
+    def __init__(self, nombre: str, edad: int, nacionalidad: str, notas: float, universidad: str):
+        super().__init__(nombre, edad, nacionalidad)
+        self.notas = notas
+        self.universidad = universidad
+
+roberto = Empleado("Roberto", 43, "Argentino", "Programador", 100000)
+
+roberto.hablar()
